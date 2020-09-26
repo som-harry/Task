42310968e014079be94a6946a2d39f4d33978df8
chisomekeh71 <chisomekeh71@gmail.com>
2020-09-26 08:08:33 -0800

    The implementation of shop management system

---
+++ HarryStoreApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HarryStoreApp.Startup))]
+namespace HarryStoreApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
